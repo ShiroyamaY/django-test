f3632b6e2c84841f4b0f1bb8b38fa40705cfd221
pavel.doicov <pavel.doicov@ebs-integrator.com>
2025-07-31 16:20:54 +0300

    Initial commit

---
+++ tms/users/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from tms.users.serializers import UserSerializer
+
+
+class RegisterUserView(GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        user = User.objects.create_user(**validated_data)
+        return Response(self.serializer_class(user).data)

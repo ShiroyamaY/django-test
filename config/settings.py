5acbbf34a72eefebbead23ddc93450471dbba48b
pavel.doicov <pavel.doicov@ebs-integrator.com>
2025-07-31 16:32:44 +0300

    feat: drf spectacular configuration

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}

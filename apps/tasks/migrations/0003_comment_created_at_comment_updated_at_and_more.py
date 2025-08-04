3d3dc772b28909fc460d38f09535015e85b21bd6
pavel.doicov <pavel.doicov@ebs-integrator.com>
2025-08-04 12:06:09 +0300

    fix: comment migration

--- apps/tasks/migrations/0003_comment_created_at_comment_updated_at_and_more.py
+++ apps/tasks/migrations/0003_comment_created_at_comment_updated_at_and_more.py
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="task",
             name="created_at",
-            field=models.DateTimeField(auto_now_add=True, default="08.03.2025"),
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
             preserve_default=False,
         ),
         migrations.AddField(

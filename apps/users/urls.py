69cb302bfc4ead01b6657fc5d82fa022648a26a7
pavel.doicov <pavel.doicov@ebs-integrator.com>
2025-08-04 12:12:24 +0300

    feat: user logged time in last month endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.users.views import RegisterUserView, UserListView
+from apps.users.views import RegisterUserView, UserListView, UserMonthlyLoggedTimeView
 
 urlpatterns = [
     path("register", RegisterUserView.as_view(), name="token_register"),
     path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logged-time/last-month", UserMonthlyLoggedTimeView.as_view(), name="logged_time_last_month"),
     path("", UserListView.as_view(), name="user_list"),
 ]
